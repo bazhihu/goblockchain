0c437c970c8583690e14134386d5f8743e30cb1a
bazhihu <435979096@qq.com>
2023-05-30 15:03:45 +0000

    ad

--- text.sh
+++ text.sh
@@ -7,3 +7,20 @@ mkdir -p tmp/ref_list
 rm main
 
 go build main.go
+
+./main createblockchain -address LeoCao
+./main blockchaininfo
+./main balance -address LeoCao
+./main send -from LeoCao -to Krad -amount 100
+./main balance -address Krad
+./main mine
+./main blockchaininfo
+./main balance -address LeoCao
+./main balance -address Krad
+./main send -from LeoCao -to Exia -amount 100
+./main send -from Krad -to Exia -amount 30
+./main mine
+./main blockchaininfo
+./main balance -address LeoCao
+./main balance -address Krad
+./main balance -address Exia
